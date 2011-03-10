391f44288dd47a69da5603b3bcce99a2a637415d
Tony Strauss <tony@blackberry.local>
2011-03-10 13:19:13 -0500

    Bump the version to 1.2.3 (must find a way to only have the version number in one place).

--- cli.sh
+++ cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG -cp target/animoto-api-1.2.2-jar-with-dependencies.jar com.animoto.api.cli.ApiClientCli $*
+java -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG -cp target/animoto-api-1.2.3-jar-with-dependencies.jar com.animoto.api.cli.ApiClientCli $*
--- src/main/java/com/animoto/api/ApiClient.java
+++ src/main/java/com/animoto/api/ApiClient.java
@@ -104,7 +104,7 @@ public class ApiClient {
   }
 
   public String getVersion() {
-    return "1.2.2";
+    return "1.2.3";
   }
 
   public String getUserAgent() {
