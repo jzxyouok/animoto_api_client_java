58aa74d41b46e671867bed320f672b8ef96a8ceb
Sander Hartlage <sander@animoto.com>
2012-01-11 14:51:13 -0500

    added more styles

--- src/main/java/com/animoto/api/enums/Style.java
+++ src/main/java/com/animoto/api/enums/Style.java
@@ -1,7 +1,12 @@
 package com.animoto.api.enums;
 
 public enum Style {
-  ORIGINAL("original");
+  ORIGINAL("original"),
+  MOTHERS_DAY_2011("mothers_day_2011"),
+  ELEGANCE("elegance_12"),
+  VINTAGE("vintage"),
+  COSMIC_TIDINGS("cosmic_tidings"),
+  WONDERLAND_OF_SNOW("wonderland_of_snow");
 
   private String value;
 
