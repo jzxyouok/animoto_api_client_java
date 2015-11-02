21cab22867fb49f70a32bab7a11a8f80ce532a3b
Tian Xie <tian@animoto.com>
2015-11-02 18:52:10 -0500

    [AP-3431] Remove real s3 credentials
    Impacted test URL's are not real anyway so there should be no impact.

--- src/test/java/com/animoto/api/resource/VideoTest.java
+++ src/test/java/com/animoto/api/resource/VideoTest.java
@@ -12,8 +12,8 @@ public class VideoTest extends TestCase {
   public void testFromJson() {
     String videoUrl = "https://api2-sandbox.animoto.com/videos/4ced2532f0c886406200062d";
     String storyboardUrl = "https://api2-sandbox.animoto.com/storyboards/4ced252cf0c88655f6000002";
-    String downloadUrl = "http://testconsumer1-4c64-sa-0.s3.amazonaws.com/Video/4ced2532f0c886406200062d/720p_3c2a.mp4?Signature=ExDB6T%2F15ldF9mcwBfCz1XIXIl0%3D&Expires=1290631624&AWSAccessKeyId=AKIAI7FBSVU753FBARAQ";
-    String coverImageUrl = "http://animototest-4c17-st-0.s3.amazonaws.com/Video/4d8ccadab3783209e3000005/cover_image.jpg?Signature=4V5xgcgVPur7E0qbpx7lQPv7w4A%3D&Expires=1301094239&AWSAccessKeyId=AKIAI7FBSVU753FBARAQ;";
+    String downloadUrl = "http://testconsumer1-4c64-sa-0.s3.amazonaws.com/Video/4ced2532f0c886406200062d/720p_3c2a.mp4?Signature=ExDB6T%2F15ldF9mcwBfCz1XIXIl0%3D&Expires=1290631624&AWSAccessKeyId=somekey";
+    String coverImageUrl = "http://animototest-4c17-st-0.s3.amazonaws.com/Video/4d8ccadab3783209e3000005/cover_image.jpg?Signature=4V5xgcgVPur7E0qbpx7lQPv7w4A%3D&Expires=1301094239&AWSAccessKeyId=somekey";
 
     Video video = new Video();
     String json = "{\"response\":{\"payload\":{\"video\":{\"metadata\":{\"rendering_parameters\":{\"resolution\":\"720p\",\"format\":\"h264\",\"framerate\":30}},\"links\":{\"self\":\"" + videoUrl + "\",\"storyboard\":\"" + storyboardUrl + "\",\"file\":\"" + downloadUrl + "\",\"cover_image\":\"" + coverImageUrl + "\"}}},\"status\":{\"code\":200}}}";
